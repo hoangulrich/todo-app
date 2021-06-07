52a942b6206754caa1e5e2228d8910009c0758bb
Hoang Le <artstyle0416@gmail.com>
2021-06-07 08:19:50 +0700

    react init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div> 
+      <Header />
+      <p>This is new app hello</p>
+    </div>
+  )
+}
+
+export default App;
