c499c9b5621a0a3cc8b6ddccdab77dac41b38de4
Hoang Le <artstyle0416@gmail.com>
2021-06-12 03:03:25 +0700

    finish

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,55 +1,67 @@
-import React, {Fragment, useState} from 'react'
-import TodoItem from './TodoItem'
+import React, { Fragment, useState } from "react";
+import TodoItem from "./TodoItem";
+import AddTodo from "./AddTodo";
+import { v4 as uuidv4 } from 'uuid';
 
 const Todos = () => {
-   const [todosState, setTodosState] = useState([
-      {
-         id: 1,
-         title: "Todo 1",
-         completed: false
-      },
-      {
-         id: 2,
-         title: "Todo 2",
-         completed: false
-      },
-      {
-         id: 3,
-         title: "Todo 3",
-         completed: false
-      }
-   ])
-   
-   const markComplete = id => {
-      const newTodos = todosState.map(todo => {
-         if (todo.id === id) todo.completed = !todo.completed
-         return todo
-      })
+  const [todosState, setTodosState] = useState([
+    {
+      id: uuidv4(),
+      title: "Todo 1",
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      title: "Todo 2",
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      title: "Todo 3",
+      completed: false,
+    },
+  ]);
 
-      setTodosState(newTodos)
-   }
+  const markComplete = (id) => {
+    const newTodos = todosState.map((todo) => {
+      if (todo.id === id) todo.completed = !todo.completed;
+      return todo;
+    });
 
-   const deleteTodo = id => {
-      const newTodos = todosState.filter(todo => {
-         return todo.id !== id
-      })
-      setTodosState(newTodos)
-   }
+    setTodosState(newTodos);
+  };
 
-   return (
-      <Fragment>
-       {todosState.map(todo => {
-           return ( 
-           <TodoItem 
-               key = {todo.id} 
-               todoProps={todo} 
-               markCompleteFunc = {markComplete}
-               deleteTodoFunc = {deleteTodo}
-           />
-           )
-       })}
-      </Fragment>
-   )
-}
+  const deleteTodo = (id) => {
+    const newTodos = todosState.filter((todo) => {
+      return todo.id !== id;
+    });
+    setTodosState(newTodos);
+  };
 
-export default Todos
+  const addTodo = title => {
+     const newTodos = [...todosState, {
+        id: uuidv4(),
+        title,
+        completed: false
+     }]
+     setTodosState(newTodos)
+  }
+
+  return (
+    <Fragment>
+      <AddTodo addTodoFunc = {addTodo}/>
+      {todosState.map((todo) => {
+        return (
+          <TodoItem
+            key={todo.id}
+            todoProps={todo}
+            markCompleteFunc={markComplete}
+            deleteTodoFunc={deleteTodo}
+          />
+        );
+      })}
+    </Fragment>
+  );
+};
+
+export default Todos;
